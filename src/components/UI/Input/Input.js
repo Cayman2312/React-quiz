f1888422ed4ee5c6535ca1062ba46d729f8003fd
M.Studitskiy <cayman2312@gmail.com>
2020-04-21 17:57:31 +0300

    валидация инпутов, подготовка

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import classes from './Input.module.scss';
+
+const Input = (props) => {
+  const inputType = props.type || 'text';
+  const cls = [classes.Input];
+  const htmlFor = `${inputType}-${Math.random()}`;
+
+  if (isInvalid(props)) {
+    cls.push(classes.invalid);
+  }
+
+  function isInvalid({ valid, touched, shouldValidate }) {
+    return !valid && touched && shouldValidate;
+  }
+
+  return (
+    <div className={cls.join(' ')}>
+      <label htmlFor={htmlFor}>{props.label}</label>
+      <input
+        id={htmlFor}
+        type={inputType}
+        value={props.value}
+        onChange={props.onChange}
+      ></input>
+      {isInvalid(props) ? (
+        <span>{props.errorMessage || 'Введите корректное значение'}</span>
+      ) : null}
+    </div>
+  );
+};
+
+export default Input;
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,90 @@
 import React, { Component } from 'react';
 import classes from './Auth.module.scss';
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 
 export default class Auth extends Component {
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'email',
+        errorMessage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+      },
+    },
+  };
+
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  onChangeHandler = (event, controlName) => {
+    console.log(`${controlName}: ${event.target.value}`);
+  };
+
+  renderInputs() {
+    let inputs = Object.keys(this.state.formControls);
+
+    inputs = inputs.map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          errorMessage={control.errorMessage}
+          shouldValidate={!!control.validation}
+          onChange={(event) => this.onChangeHandler(event, controlName)}
+        />
+      );
+    });
+
+    return inputs;
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+
+          <form className={classes.AuthForm} onSubmit={this.submitHandler}>
+            {this.renderInputs()}
+
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
