f05fe0951ca4e64a6207cbae3c5bce1324523ec1
M.Studitskiy <cayman2312@gmail.com>
2020-04-21 09:03:56 +0300

    начало оформления меню

---
+++ src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Drawer.module.scss';
+
+const Drawer = () => {
+  return <div></div>;
+};
+
+export default Drawer;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import classes from './Layout.module.scss';
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle';
 
 class Layout extends React.Component {
+  state = {
+    menu: false,
+  };
+
+  loggleMenuHandler = () => {
+    this.setState({ menu: !this.state.menu });
+  };
+
   render() {
     return (
       <div className={classes.Layout}>
+        <MenuToggle
+          onToggle={this.loggleMenuHandler}
+          isOpen={this.state.menu}
+        />
         <main>{this.props.children}</main>
       </div>
     );
