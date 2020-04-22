e5b431978629a50ab605acafd48d52379db3ab72
M.Studitskiy <cayman2312@gmail.com>
2020-04-22 16:16:10 +0300

    добавлена регистрация и авторизация на firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import classes from './Auth.module.scss';
 import is from 'is_js';
+import axios from 'axios';
 
 export default class Auth extends Component {
   state = {
@@ -35,9 +36,39 @@ export default class Auth extends Component {
     },
   };
 
-  loginHandler = () => { };
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    };
+    try {
+      const response = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD2Ugu2L-OBoPuTwwoONz0yA11-kLSvvwY',
+        authData
+      );
+      console.log('response.data', response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-  registerHandler = () => { };
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    };
+    try {
+      const response = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD2Ugu2L-OBoPuTwwoONz0yA11-kLSvvwY',
+        authData
+      );
+      console.log('response.data', response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   submitHandler = (e) => {
     e.preventDefault();
@@ -78,9 +109,8 @@ export default class Auth extends Component {
     isFormValid = Object.values(formControls).every((value) => value.valid);
 
     if (isFormValid) {
-      this.setState({ isFormValid: true })
+      this.setState({ isFormValid: true });
     }
-
   };
 
   renderInputs() {
@@ -115,10 +145,18 @@ export default class Auth extends Component {
           <form className={classes.AuthForm} onSubmit={this.submitHandler}>
             {this.renderInputs()}
 
-            <Button type="success" onClick={this.loginHandler} disabled={!this.state.isFormValid}>
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
+            >
               Войти
             </Button>
-            <Button type="primary" onClick={this.registerHandler} disabled={!this.state.isFormValid}>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
+            >
               Зарегистрироваться
             </Button>
           </form>
