60d66289b272e9bb67fa3a5f4e486441fe736092
M.Studitskiy <cayman2312@gmail.com>
2020-04-22 14:50:28 +0300

    render тестов из базы

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -4,14 +4,37 @@ import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 
 export default class QuizList extends Component {
+  state = {
+    quizes: [],
+  };
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map((quiz) => {
       return (
-        <li key={index}>
-          <NavLink to={`/quiz/${quiz}`}>Тест {quiz}</NavLink>
+        <li key={quiz.id}>
+          <NavLink to={`/quiz/${quiz.id}`}>{quiz.name}</NavLink>
         </li>
       );
     });
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        'https://quiz-5af9e.firebaseio.com/quizes.json'
+      );
+      const quizes = [];
+
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Тест №${index + 1}`,
+        });
+      });
+      this.setState({ quizes });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   render() {
